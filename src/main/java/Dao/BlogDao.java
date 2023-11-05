c5368ced04c1e69fec8d5bb108505ef9c5bf4b46
haoyun0 <847360401@qq.com>
2023-11-06 00:58:10 +0800

    fix bugs

--- src/main/java/Dao/BlogDao.java
+++ src/main/java/Dao/BlogDao.java
@@ -14,14 +14,14 @@ public class BlogDao {
     // 1. 向博客列表中插入一个博客
     public void insert(Blog blog) throws SQLException {
         // JDBC 基本代码
-        Connection connection=null;
-        PreparedStatement statement=null;
+        Connection connection = null;
+        PreparedStatement statement = null;
         try {
             // ①和数据库建立连接.
             connection = DBUtil.getConnection();
             // ② 构造 SQL 语句
             String sql = "insert into blog values(null, ?, ?, ?, now())";
-            statement=connection.prepareStatement(sql);
+            statement = connection.prepareStatement(sql);
             //第一个参数表示的是第几个？的位置
             statement.setString(1, blog.getTitle());
             statement.setString(2, blog.getContent());
@@ -32,23 +32,23 @@ public class BlogDao {
             e.printStackTrace();
         } finally {
             // ④ 关闭连接, 释放资源
-            DBUtil.close(connection,statement,null);
+            DBUtil.close(connection, statement,null);
         }
     }
     // 2. 能够获取到博客表中的所有博客的信息 (用于在博客列表页, 此处每篇博客不一定会获取到完整的正文)
     public List<Blog> selectAll() throws SQLException {
         List<Blog>blogs = new ArrayList<>();
-        Connection connection=null;
-        PreparedStatement statement=null;
-        ResultSet resultSet=null;
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
         try {
-            connection=DBUtil.getConnection();
-            String sql="select * from blog";
-            statement=connection.prepareStatement(sql);
-            resultSet=statement.executeQuery();
+            connection = DBUtil.getConnection();
+            String sql = "select * from blog";
+            statement = connection.prepareStatement(sql);
+            resultSet = statement.executeQuery();
             //遍历整个集合
             while(resultSet.next()){
-                Blog blog=new Blog();
+                Blog blog = new Blog();
                 blog.setBlogId(resultSet.getInt("id"));
                 blog.setTitle(resultSet.getString("title"));
                 blog.setContent(resultSet.getString("content"));
@@ -66,18 +66,18 @@ public class BlogDao {
 
     // 3. 能够根据博客 id 获取到指定的博客内容 (用于在博客详情页)
     public Blog selectOne(int blogId) throws SQLException {
-        Connection connection=null;
-        PreparedStatement statement=null;
-        ResultSet resultSet=null;
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
         try {
-            connection=DBUtil.getConnection();
-            String sql="select * from blog where id=?";
-            statement=connection.prepareStatement(sql);
+            connection = DBUtil.getConnection();
+            String sql = "select * from blog where id=?";
+            statement = connection.prepareStatement(sql);
             statement.setInt(1,blogId);
-            resultSet=statement.executeQuery();
+            resultSet = statement.executeQuery();
             //由于id是唯一的，要么是0条，要么是唯一的，是主键作为查询条件的，所以直接用if
             if (resultSet.next()){
-                Blog blog=new Blog();
+                Blog blog = new Blog();
                 blog.setBlogId(resultSet.getInt("id"));
                 blog.setTitle(resultSet.getString("title"));
                 blog.setContent(resultSet.getString("content"));
@@ -94,11 +94,12 @@ public class BlogDao {
     }
     // 4. 从博客表中, 根据博客 id 删除博客.
     public void delete(int blogId) throws SQLException {
-        Connection connection=null;
-        PreparedStatement statement=null;
+        Connection connection = null;
+        PreparedStatement statement = null;
         try {
-            connection=DBUtil.getConnection();
-            String sql="delete from blog where id=?";
+            connection = DBUtil.getConnection();
+            String sql = "delete from blog where id=?";
+            statement = connection.prepareStatement(sql);
             statement.setInt(1,blogId);
             statement.executeUpdate();
         } catch (SQLException e) {
